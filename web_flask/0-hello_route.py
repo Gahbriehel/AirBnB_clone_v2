78d2e0bac77f6f3458e5bf73380b9035dd164e12
gahbriehel <babatise002@gmail.com>
2024-08-23 00:03:22 +0100

    fixed pycodestyle issues

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+This module is to create a basic flask app
+"""
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,6 +9,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def home():
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
